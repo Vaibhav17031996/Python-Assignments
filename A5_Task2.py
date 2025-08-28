1013ee0e10dd7c35282adc4f0a00fe87421977aa
Vaibhav17031996 <vaibhavpatwal3@gmail.com>
2025-08-28 16:23:58 +0530

    A5_Task2 done

---
+++ A5_Task2.py
@@ -0,0 +1,12 @@
+list = list(range(1, 11))
+extracted_list = list[:5]
+reversed_list = extracted_list[::-1]
+
+# Aliter: Using built-in functions
+# reversed_list = list(reversed(extracted_list))
+# extracted_list.reverse()    // extracted_list itself is changed (no separate variable(reversed_list) is required).
+
+
+print(f"Original list: {list}")
+print(f"Extracted first five elements: {extracted_list}")
+print(f"Reversed extracted elements: {reversed_list}")
