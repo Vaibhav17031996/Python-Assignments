fbc210ab2bb6fb1b6041b0098fe08b351932fade
Vaibhav17031996 <vaibhavpatwal3@gmail.com>
2025-08-28 17:28:40 +0530

    done

--- A5_Task2.py
+++ A5_Task2.py
@@ -6,7 +6,6 @@ reversed_list = extracted_list[::-1]
 # reversed_list = list(reversed(extracted_list))
 # extracted_list.reverse()    // extracted_list itself is changed (no separate variable(reversed_list) is required).
 
-
 print(f"Original list: {list}")
 print(f"Extracted first five elements: {extracted_list}")
 print(f"Reversed extracted elements: {reversed_list}")
